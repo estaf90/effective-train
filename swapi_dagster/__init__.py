c648c7073d552c15cc50c6cd2456247a5c14095c
Emil Staf <emil.staf@gmail.com>
2023-10-02 13:56:26 +0200

    Start pipeline by default

--- swapi_dagster/__init__.py
+++ swapi_dagster/__init__.py
@@ -2,6 +2,7 @@ from dagster import (
     AssetSelection,
     Definitions,
     ScheduleDefinition,
+    DefaultScheduleStatus,
     define_asset_job,
     load_assets_from_modules,
 )
@@ -14,7 +15,8 @@ all_assets_job = define_asset_job("all_assets_job", selection=AssetSelection.all
 
 all_assets_schedule = ScheduleDefinition(
     job=all_assets_job,
-    cron_schedule="0 0 * * *",  # Run job 00:00 every day.
+    cron_schedule="@daily",
+    default_status=DefaultScheduleStatus.RUNNING,
 )
 
 defs = Definitions(
